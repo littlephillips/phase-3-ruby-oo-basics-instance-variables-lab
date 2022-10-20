799209212d1f94e389c687dc554c736c39c35c5c
littlephillips <phillipkinuthia72@gmail.com>
2022-10-21 00:26:41 +0300

    instance variable

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,14 @@
 class Dog
+  def name=(dog_name)
+    @this_dogs_name = dog_name
+  end
 
+  def name
+    @this_dogs_name
+  end
 end
+
+Phillip = Dog.new
+Phillip.name = "Phillip"
+
+puts Phillip.name 
